b513dc13a00638634d1777be3ec7c4377c390103
Saragam Ghimire <codewithghimire@gmail.com>
2023-02-15 21:47:10 +0545

    changed screen size and hello page work up to lg screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,33 +4,34 @@ module.exports = {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    screens:{
-      brp:'290px',
-      sm:'485px',
-      md:'768px',
-      lg:'976px',
-      xl:'1440px'
+    screens: {
+      brp: '290px',
+      sm: '485px',
+      md: '768px',
+      lg:'860px',
+      xl: '976px',
+      xxl: '1440px',
     },
     extend: {
-      colors:{
-        darkBlack:"#1E1E1E",
-        lightBlack:"#2D2D2D",
-        normalBlack:"#252526",
-        grayishWhite:"#8E9093",
-        textWhite:"#CCCCCC",
-        brightWhite:"#FFFFFF",
-        brightYellow:"#E8AE39",
-        skyBlue:"#6099BD"
+      colors: {
+        darkBlack: "#1E1E1E",
+        lightBlack: "#2D2D2D",
+        normalBlack: "#252526",
+        grayishWhite: "#8E9093",
+        textWhite: "#CCCCCC",
+        brightWhite: "#FFFFFF",
+        brightYellow: "#E8AE39",
+        skyBlue: "#6099BD"
       },
-      fontFamily:{
-        "inter":['"Inter"','sans-serif'],
-        "firaCode":['"Fira Code"','monospace']
+      fontFamily: {
+        "inter": ['"Inter"', 'sans-serif'],
+        "firaCode": ['"Fira Code"', 'monospace']
       },
-      width:{
-        '161':'161px'
+      width: {
+        '161': '161px'
       },
-      height:{
-        '47':'47px'
+      height: {
+        '47': '47px'
       }
     },
   },
