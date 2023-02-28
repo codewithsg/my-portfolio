fc887243960a4f204fcea904497aeb28ba21e1af
Saragam Ghimire <codewithghimire@gmail.com>
2023-02-28 23:19:52 +0545

    nav bar hamburger button moved to middle

--- src/app/shared/nav-bar/nav-bar.component.ts
+++ src/app/shared/nav-bar/nav-bar.component.ts
@@ -13,13 +13,13 @@ export class NavBarComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    const btn = document.getElementById('menu-btn');
-    const nav = document.getElementById('menu');
-    btn?.addEventListener('click', () => {
-      btn?.classList.toggle('open')
-      nav?.classList.toggle('flex')
-      nav?.classList.toggle('hidden')
-    })
+    // // const btn = document.getElementById('menu-btn');
+    // const nav = document.getElementById('menu');
+    // // btn?.addEventListener('click', () => {
+    // //   btn?.classList.toggle('open')
+    //   // nav?.classList.toggle('flex')
+    //   nav?.classList.toggle('hidden')
+    // })
   }
 
   hamburgerMenu(){
