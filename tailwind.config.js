4f5f435bb1dd542caaa7f34d231e7e8e89b25b31
Saragam Ghimire <codewithghimire@gmail.com>
2023-02-28 20:22:56 +0545

    hello component ismade responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     screens: {
       'xs':'480px',
       'sm':'640px',
-      'md':'768px',
+      'md':'800px',
       'lg':'1024px',
       'xl':'1280px',
       'xxl':'1536px'
