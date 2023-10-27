a73b908431cd7a5a9f8d455bf05cae2ccaea3789
Saragam Ghimire <codewithghimire@gmail.com>
2023-10-27 15:57:13 +0545

    left border line implemented to all except home page

--- src/app/project/two-three/two-three.component.ts
+++ src/app/project/two-three/two-three.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./two-three.component.scss']
 })
 export class TwoThreeComponent {
+  projects = [
+    {
+      name: 'Human Resource Management System',
+      skills: ['Ionic VueJs', 'Laravel', 'MySql'],
+      duration: 'Dec 2022 - Jan 2023',
+      description: 'Human Resource Management is a mobile application that streamlines attendance management, leave requests and provides insightful reports. Employees can quickly and easily log their attendance and system saves their location to ensure they are logging from designated work area. Additionally employees can request leaves directly from app, which saves time and simplify the process. This app also keep track of workdays, so that employees can plan their time off accordingly. The system was mainly designed to make attendance and leave management simple, efficient and user-friendly.',
+      isAvailable: false,
+      url: 'undefined'
+    }
+  ]
 
+  openProject(url: string) {
+    window.open(url, '_blank');
+  }
 }
