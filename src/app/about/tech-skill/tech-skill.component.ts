0a60cf7cb44974e565db9fbd7a7529d7e708144d
Saragam Ghimire <codewithghimire@gmail.com>
2023-10-27 11:49:31 +0545

    tech skill left border added

--- src/app/about/tech-skill/tech-skill.component.ts
+++ src/app/about/tech-skill/tech-skill.component.ts
@@ -8,9 +8,57 @@ import { Component } from '@angular/core';
 export class TechSkillComponent {
   techSkills = [
     {
-      logo:'./../../../assets/img/typescript.svg',
-      name:'TypeScript',
-      skill:''
+      logo: './../../../assets/img/typescript.svg',
+      name: 'TypeScript',
+      skill: 4
+    }, {
+      logo: './../../../assets/img/nestjsLogo.svg',
+      name: 'NestJs',
+      skill: 3
+    }, {
+      logo: './../../../assets/img/mongodbLogo.svg',
+      name: 'MongoDB',
+      skill: 3
+    }, {
+      logo: './../../../assets/img/mysqlLogo.svg',
+      name: 'MySQL',
+      skill: 3
+    }, {
+      logo: './../../../assets/img/postgresql.svg',
+      name: 'PostgreSQL',
+      skill: 4
+    }, {
+      logo: './../../../assets/img/jestLogo.svg',
+      name: 'Jest',
+      skill: 4
+    }, {
+      logo: './../../../assets/img/awsLogo.svg',
+      name: 'Amazon Web Service',
+      skill: 3
+    }, {
+      logo: './../../../assets/img/gitLogo.svg',
+      name: 'Git',
+      skill: 4
+    }, {
+      logo: './../../../assets/img/angularLogo.svg',
+      name: 'Angular',
+      skill: 3
+    }, {
+      logo: './../../../assets/img/reactLogo.svg',
+      name: 'React',
+      skill: 3
+    }, {
+      logo: './../../../assets/img/vuejsLogo.svg',
+      name: 'VueJS',
+      skill: 3
+    }, {
+      logo: './../../../assets/img/laravelLogo.svg',
+      name: 'Laravel',
+      skill: 2
     }
   ]
+
+  getStars(skillRating:number):number[] {
+    return new Array(skillRating);
+  }
 }
